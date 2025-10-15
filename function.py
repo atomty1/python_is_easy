95298d918abcecdc90a97ffdf017d06db77e3da3
Taye Abidakun <tayeabidakun@gmail.com>
2025-10-15 18:50:32 +0100

    python function

---
+++ function.py
@@ -0,0 +1,59 @@
+# print("Welcome to my calculator")
+# def calculator():
+#     print("What do you want to do?")
+#     menu = input("1. Addition 2. Subtraction 3. Multiplication 4. Division: ")
+#     if(menu == "1"):
+#         addition()
+#     elif(menu == "2"):
+#         subtraction()
+#     elif(menu == "3"):
+#         multiply()
+#     elif(menu == "4"):
+#         divide()
+#     else:
+#         print("Invalid input")
+    
+#     print("Do you want to do any other thing?")
+#     proceed = input("y. Yes n. No: ")
+#     if(proceed == "y"):
+#         calculator()
+#     else:
+#         print("Byeee!")
+    
+
+
+# def addition():
+#     first_number = float(input("Enter the first number: "))
+#     second_number = float(input("Enter the second number: "))
+#     result = first_number + second_number
+#     print(result)
+
+# def subtraction():
+#     first_number = float(input("Enter the first number: "))
+#     second_number = float(input("Enter the second number: "))
+#     result = first_number - second_number
+#     print(result)
+
+# def multiply():
+#     first_number = float(input("Enter the first number: "))
+#     second_number = float(input("Enter the second number: "))
+#     result = first_number * second_number
+#     print(result)
+
+# def divide():
+#     first_number = float(input("Enter the first number: "))
+#     second_number = float(input("Enter the second number: "))
+#     result = first_number / second_number
+#     print(result)
+
+# calculator()
+
+# Weight
+# height
+
+# bmi = weight/(height * height)
+# less than 18 = underweight
+# 18 - 25 = Normal weight
+# 25 - 30 - Overweight
+# > 30 - Obese
+# 1. Yes 2. No
--- if.py
+++ if.py
@@ -1,48 +1,17 @@
-# a = 2
-# if (a < 12):
-#     print("First")
-# elif(a < 5):
-#     print("Second")
-# elif( a < 3):
-#     print("Third")
-# if(a == 2):
-#     print("Fourth")
-
-# b = 7
-# if (b != 4):
-#     print("Welcome")
-# else:
-#     print("Heeey")
-
-# c = 4
-# d = 8
-# e = 5
-
-# if( c < 10 or d == 8 and e > 15):
-#     print("Hello")
-# else:
-#     print("HI")
-print("Signup")
-username = input("Enter your username: ")
-
-print("Signup successful.")
-repeat_username = input("Enter your username: ")
-
-if(username == repeat_username):
-    password = input("Enter your password: ")
-    repeat_password = input("Enter your password: ")
-    if(password == repeat_password):
-        print("Password match")
-    
-    else:
-        print("Password do not match")
-else:
-    print("Incorrect username")
-
-# 70 - 100 - Excellent
-# 60 - 69 - Good
-# 50 - 59 - average 
-# 45 - 49 - Fair
-# 40 - 44 - Poor
-# 0 - 39 - Fail
-# More than 100 or less than 0 - Out of range
+print("welcome to a new session")
+print("Find out your grade")
+user_score = int(input("Enter your score: "))
+if(user_score < 0 or user_score > 100):
+    print("Out of range")
+elif(user_score <= 39):
+    print("fail")
+elif(user_score <= 44):
+    print("Poor")
+elif(user_score <= 49):
+    print("Fair")
+elif(user_score <= 59):
+    print("Average")
+elif(user_score <= 69):
+    print("Good")
+elif(user_score <= 100):
+    print("Excellent")
