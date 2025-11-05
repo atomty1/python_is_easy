8356c233d4774f063df75f6d6a2cd745c458084c
Taye Abidakun <tayeabidakun@gmail.com>
2025-11-05 18:37:32 +0100

    loop

--- loop.py
+++ loop.py
@@ -1,7 +1,48 @@
-start = int(input("Enter starting point: "))
-end = int(input("Enter the ending point: "))
-while start <= end:
-    print(start)
-    start = start + 1
-    
+# actual_num = input("Enter the actual number: ")
+# guessed_num = input("Guess a number: ")
 
+# while num != "10":
+#     print("Wrong")
+#     num = input("Guess another number: ")
+# print("Correct")
+# Assignment 
+# 20
+# 15
+# wrong
+# Move higher
+# 50
+# wrong
+# Move lower
+
+# 40
+# Wrong
+# Move lower
+# 18
+# Wrong Higher
+# 30
+# Wrong
+# Move lower
+# 20
+# Correct
+# break
+# i = 4
+# while i < 10:
+#     print(i)
+#     if(i == 7):
+#         break
+#     i = i + 1
+# 5, 6, 7, 9
+# i = 5
+# while i < 10:
+#     print("Hello")
+#     if i == 8:
+#         i = i + 1
+#         continue
+#     print(i)
+#     i = i + 1
+
+# Assignment 2:
+# enter the starting point 20
+# Enter the ending point   35
+# Enter the number you want to skip 33
+    
