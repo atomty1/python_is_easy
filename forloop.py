f0ee9de323c5ac1328024c7a356750d425862ba8
Taye Abidakun <tayeabidakun@gmail.com>
2025-11-12 18:40:59 +0100

    indeex and values in a list

--- forloop.py
+++ forloop.py
@@ -19,4 +19,28 @@
 
 # print(f"The sum of all the values in the list is {total}")
 
+# start = int(input("Enter the starting point: ")) 
+# end = int(input("Enter the ending point: ")) 
+# skip = int(input("Enter the number you want to skip: "))
 
+# while start <= end:
+#     if (start == skip):
+#         start = start + 1
+#         continue
+#     print(start)
+#     start = start + 1
+
+
+# nums = [15, 12, 6, 20, 7]
+# max = nums[0]
+# for num in nums:
+#     if( num > max ):
+#         max = num
+
+# print(f"The maximum number is {max}")
+names = ["Taye", "Kehinde", "Taye", "Tunde", "Chinedu", "Ajayi"]
+# for name in names:
+#     print(f"{name} is at position ")
+names_length = len(names)
+for i in range(names_length): # 0, 1
+    print(f"{names[i]} is at position {i}")
