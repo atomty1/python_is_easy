f9db1b094d1335c51b6ad8a10a7f3f551cb43a38
Taye Abidakun <tayeabidakun@gmail.com>
2025-12-11 18:41:28 +0100

    lambda function in python

---
+++ lambda.py
@@ -0,0 +1,23 @@
+# def multiply_by_2(val):
+#     return val * 2
+
+# print(multiply_by_2(10))
+# a = lambda val, num : val * num
+# print(a(10, 20))
+# nums = [10, 20, 30]
+# result = list(map(lambda val : val - 5, nums))
+# print(result)
+# result = []
+# for num in nums:
+#     result.append(num - 5)
+
+# print(result)
+# res = tuple(map(lambda val: val > 7, [5, 8, 2, 15]))
+# print(res)
+
+# a = list(map(lambda val : "Taye", [9, 10, 20]))
+# names = ["Tolu", "Ojo", "Abidakun", "Alekuwodo", "abcdef"]
+# result = filter(lambda name: len(name) > 5, names)
+# res = list(map(lambda taye : len(taye), result))
+# print(res)
+
